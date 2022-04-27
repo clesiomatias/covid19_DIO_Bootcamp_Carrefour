4c2700b37a24beeba965a9881a2ee1da065fda33
clesiomatias <clesiofmatias@gmail.com>
2022-04-26 21:27:47 -0300

    Parte 2 completa

--- covid19_dio/src/App.js
+++ covid19_dio/src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
-
+import React from "react";
+import { StylesProvider } from "@material-ui/styles";
+import { CssBaseline } from "@material-ui/core";
+import GlobalStyle from "./commons/styles/global-style";
 function App() {
   return (
-   <>
-   </>
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+      <div>teste</div>
+    </StylesProvider>
   );
 }
 
 export default App;
 
-
-//pt 1 completa
+//pt 2 completa
